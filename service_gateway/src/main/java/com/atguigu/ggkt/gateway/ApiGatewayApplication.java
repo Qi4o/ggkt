c7ceec9decb1acf907512c5b73615a36c3caf139
Qi4o <1303759265@qq.com>
2022-10-12 18:03:54 +0800

    gateway

---
+++ service_gateway/src/main/java/com/atguigu/ggkt/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.ggkt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Qiao
+ * @Create 2022/10/12 12:24
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
